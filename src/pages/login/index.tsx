28711dde116e652d12a2c0d4e437cc2d84c4b482
snargio <gu200412g@gmail.com>
2022-04-05 03:34:08 -0300

    criado logacao para login peg log

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,11 +1,15 @@
+import  Logacao  from '../../components/Logacao'
 import PageHeader from '../../components/PageHeader'
 
 import './styles.css'
 
 function login() {
   return (
-    <div id="login" className="login">
-      <PageHeader />
+    <div id="login" className="container">
+      <header>
+        <PageHeader />
+      </header>
+      <Logacao />
     </div>
   )
 }
